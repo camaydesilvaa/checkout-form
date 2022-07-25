6a71a6f8a298795ac2fec0352f65297f5a269aea
camaydesilvaa <camay.desilva@cdpipelinedevshop.com>
2022-07-25 14:34:03 +0000

    first commit

---
+++ checkout/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Checkout from './components/Checkout.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Checkout></Checkout>
+    </div>
+  );
+}
+
+export default App;
